b75dcd692272f3d1b008887d0c6a2cbb6529e6c0
braga1233 <braga1233@gmail.com>
2024-12-04 12:37:18 -0300

    correção

--- commands/trazertodos.py
+++ commands/trazertodos.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-
 class TrazerTodos(commands.Cog):
   def __init__(self, client: commands.Bot):
     self.client = client
@@ -10,7 +9,14 @@ class TrazerTodos(commands.Cog):
   @app_commands.command(name='puxartodos', description="Puxa todos os usuários até o seu canal.")
   @app_commands.guild_only()
   async def trazerTodos(self, interaction: discord.Interaction):
+    bot_role = interaction.guild.me.top_role 
+    author_role = interaction.user.top_role
 
+    if author_role <= bot_role: 
+      await interaction.response.send_message( 
+        embed=discord.Embed(description="Você não tem permissão para usar este comando.", color=discord.Color.red()), ephemeral=True ) 
+      return
+  
     afkChannel = interaction.guild.afk_channel
     authorChannel = interaction.user.voice.channel
     for channel in interaction.guild.voice_channels:
@@ -18,7 +24,6 @@ class TrazerTodos(commands.Cog):
         for member in channel.members:
           await member.move_to(authorChannel)
     await interaction.response.send_message(embed=discord.Embed(description="Comando executado com sucesso!", color=interaction.guild.me.color), ephemeral=True, delete_after=4)
-
 
 async def setup(client: commands.Bot):
   await client.add_cog(TrazerTodos(client))
