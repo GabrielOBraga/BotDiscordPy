f8ed03201a3bd0a2176bd637becf873bb4195601
braga1233 <braga1233@gmail.com>
2024-12-04 12:03:25 -0300

    chance trazer todos cargos

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use uma imagem base oficial do Python
-FROM python:3.13-slim
+FROM python:3.11-slim
 
 # Defina o diretório de trabalho no contêiner
 WORKDIR /app
@@ -7,8 +7,9 @@ WORKDIR /app
 # Copie os arquivos de requisitos para o diretório de trabalho
 COPY requirements.txt .
 
+RUN pip3 install --upgrade pip
 # Instale as dependências do Python
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 # Copie o restante do código da aplicação para o diretório de trabalho
 COPY . .
--- commands/trazertodoscargos.py
+++ commands/trazertodoscargos.py
@@ -3,23 +3,24 @@ from discord.ext import commands
 from discord import app_commands
 
 
-class TrazerTodosCargo(commands.Cog):
+class TrazerTodosCargos(commands.Cog):
   def __init__(self, client: commands.Bot):
     self.client = client
 
-  @app_commands.command(name='puxartodoscargo', description="Puxa todos os usuários pelo cargo até o seu canal.")
+  @app_commands.command(name='puxarporcargo', description="Puxa todos os usuários pelo cargo até o seu canal.")
   @app_commands.guild_only()
-  async def trazerTodosCargo(self, interaction: discord.Interaction, *, cargos: discord.Role):
+  async def trazerTodosCargos(self, interaction: discord.Interaction, cargos: discord.Role):
 
     afkChannel = interaction.guild.afk_channel
     authorChannel = interaction.user.voice.channel
     for channel in interaction.guild.voice_channels:
       if not channel == authorChannel and len(channel.members) > 0 or channel == afkChannel:
         for member in channel.members:
-          if member.role == cargos.id:
-            await member.move_to(authorChannel)
+          for m_role in member.roles:
+            if m_role.id == cargos.id:
+              await member.move_to(authorChannel)
     await interaction.response.send_message(embed=discord.Embed(description="Comando executado com sucesso!", color=interaction.guild.me.color), ephemeral=True, delete_after=4)
 
 
 async def setup(client: commands.Bot):
-  await client.add_cog(TrazerTodosCargo(client))
+  await client.add_cog(TrazerTodosCargos(client))
